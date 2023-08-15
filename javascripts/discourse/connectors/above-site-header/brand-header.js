e2cc21b33261ffeb32030e8ab5ea92775c911fbc
Kris <kris.aubuchon@discourse.org>
2023-08-15 08:30:43 -0400

    DEV: refactor, adjust styles, add test, fix issues (#25)

--- javascripts/discourse/connectors/above-site-header/brand-header.js
+++ javascripts/discourse/connectors/above-site-header/brand-header.js
@@ -2,7 +2,7 @@ export default {
   setupComponent(args, component) {
     // show full bar on desktop and on mobile if specifically enabled
     component.set(
-      "showWidget",
+      "showBar",
       !component.site.mobileView || settings.show_bar_on_mobile
     );
   },
