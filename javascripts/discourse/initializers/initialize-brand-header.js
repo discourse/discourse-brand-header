f002a9350b9ca1cdb720d9d0b9da1424e2abe6a3
Sérgio Saquetim <1108771+megothss@users.noreply.github.com>
2025-08-12 18:04:49 -0300

    DEV: Remove unused widget shim (#77)
    Remove unused widget shim from `initialize-brand-header`.
    
    Adjust compatibility for `< 3.5.0.beta9-dev`.

--- javascripts/discourse/initializers/initialize-brand-header.js
+++ javascripts/discourse/initializers/initialize-brand-header.js
@@ -1,19 +1,11 @@
-import { hbs } from "ember-cli-htmlbars";
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { registerWidgetShim } from "discourse/widgets/render-glimmer";
 import BrandHeaderContainer from "../components/brand-header-container";
 
 export default {
   name: "brand-header",
 
   initialize() {
-    registerWidgetShim(
-      "brand-header-contents",
-      "div.brand-header-contents",
-      hbs`<BrandHeaderContents />`
-    );
-
-    withPluginApi("1.14.0", (api) => {
+    withPluginApi((api) => {
       api.renderInOutlet(settings.plugin_outlet, BrandHeaderContainer);
     });
   },
